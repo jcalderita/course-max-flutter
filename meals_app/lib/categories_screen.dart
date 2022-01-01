2508428bba2a4e126208c2be17ce19beda94171e
Jorge Calderita <jcalderita@gmail.com>
2022-01-01 17:43:58 +0100

    start module

---
+++ meals_app/lib/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'DeliMeal',
+        ),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25.0),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20.0,
+          mainAxisSpacing: 20.0,
+        ),
+        children: dummyCategories.map((e) => CategoryItem(e.title, e.color)).toList(),
+      ),
+    );
+  }
+}
---
+++ meals_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink).copyWith(secondary: Colors.amber),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline1: const TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
